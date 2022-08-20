49dcc1f10d160fe1d820966bf5904b7ae771f64b
Patrik Elfström <me@patrikelfstrom.se>
2022-08-20 14:33:40 +0200

    Deploy as static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
-  adapter: cloudflare(),
 });
